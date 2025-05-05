a530b0ddebac717fa65ac910045d7da96f9bb361
hrtowii <68852354+hrtowii@users.noreply.github.com>
2025-05-06 04:04:31 +0700

    first commit im sleepy

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<h1>hrt-tracker</h1>
+<p>get started with tracking your doses and blood tests!</p>
+<a href="/backup">import data</a>
+<a href="/create/blood-test">create blood test</a>
+<a href="/create/dosage">set up dosage tracker</a>
---
+++ src/routes/create/blood-test/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    let test_date = $state("");
+    let dosage_date = $state("");
+    // need global dropdown variables for different estrogen types? should i refer to transfemsci for this
+</script>
+
+<h1>create blood test entry</h1>
